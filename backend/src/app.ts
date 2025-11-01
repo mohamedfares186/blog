d5e3472f906f06a8c71f12d9b9fbc341c485103f
Mohamed Fares <fares4697@gmail.com>
2025-11-01 04:29:02 +0200

    initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import app from "./app.ts";
+
+const PORT: number = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server is running on port: ${PORT}`)
+);
